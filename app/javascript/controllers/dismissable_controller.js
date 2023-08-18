71c12642478f1dab9689d88e9c9c5f1a386ebc79
Kryptyk Physh <kryptykphysh@kryptykphysh.xyz>
2023-08-18 15:56:56 +0100

    Scaffold Scene resource Add a 10 second timeout on flash messages

--- app/javascript/controllers/dismissable_controller.js
+++ app/javascript/controllers/dismissable_controller.js
@@ -2,7 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dismissable"
 export default class extends Controller {
+  connect() {
+    setTimeout(() => this.dismiss(), 10000);
+  }
+
   dismiss() {
+    console.log("dismiss called");
     this.element.classList.add("animate__fadeOutUp");
     this.element.addEventListener("animationend", () => {
       this.element.remove();
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_172808) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_144224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "scenes", force: :cascade do |t|
+    t.citext "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_scenes_title_unique", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
